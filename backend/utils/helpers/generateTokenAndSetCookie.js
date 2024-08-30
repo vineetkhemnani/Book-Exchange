1feaec0a299d90672c24a5a2fd7a3502728601a3
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-08-30 19:03:09 +0530

    cookie policy update

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -1,14 +1,20 @@
 import jwt from 'jsonwebtoken'
+
 const generateTokenAndSetCookie = (userId, res) => {
-    // function to generate cookie based on userId as userId is unique
+  // Determine if the environment is production or development
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  // Generate the JWT token with a 15-day expiration
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '15d',
   })
-  // create a cookie(nameOfCookie,cookieValue,options)
+
+  // Create the cookie with appropriate options
   res.cookie('jwt', token, {
-    // httpOnly: true, //more secure -> cookie cannot be accessed by the browser
+    httpOnly: true, // more secure -> cookie cannot be accessed by the browser
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
-    sameSite: 'strict', //CSRF
+    sameSite: 'none', // CSRF protection
+    secure: isProduction, // cookie is sent only over HTTPS in production
   })
 
   return token
